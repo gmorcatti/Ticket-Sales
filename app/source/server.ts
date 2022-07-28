53ec2723b84edd6624ac8af6b99ab1570ff48597
Gabriel Morcatti <gabriel.morcatti@pelando.com.br>
2022-07-28 10:01:53 -0300

    feat: Instantiate Routes and ErrorHandler

--- app/source/server.ts
+++ app/source/server.ts
@@ -1,11 +1,20 @@
+import 'reflect-metadata'
 import 'dotenv/config'
 import 'express-async-errors'
 
 import express from 'express'
 
+import routes from './routes'
+
+import { ErrorHandler } from '~shared/types/errors/ErrorHandler'
+
 const app = express()
 
 app.use(express.json())
+
+app.use(routes)
+
+app.use(ErrorHandler)
 
 app.listen(3000, () => {
   console.log('Server running on port 3000')
