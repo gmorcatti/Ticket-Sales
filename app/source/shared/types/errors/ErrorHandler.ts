4be22c60098145c7b3783828416d4f85d83c5095
Gabriel Morcatti <gabriel.morcatti@pelando.com.br>
2022-07-28 09:58:24 -0300

    feat: Create custom Error and ErrorHandler

---
+++ app/source/shared/types/errors/ErrorHandler.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from 'express'
+
+import { AppError } from './AppError'
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const ErrorHandler = (err: Error, _: Request, res: Response, __: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).send({
+      message: err.message,
+    })
+  }
+
+  return res.status(500).send({
+    message: 'Internal Server Error',
+    details: err.message,
+  })
+}
