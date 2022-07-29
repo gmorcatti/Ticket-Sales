82f64706bd76807e742ca2c02a7af92a2f539e70
Gabriel Morcatti <gabriel.morcatti@yahoo.com.br>
2022-07-29 10:07:45 -0300

    refactor: Create Event return ID

--- app/source/modules/events/repositories/IEventsRepository.ts
+++ app/source/modules/events/repositories/IEventsRepository.ts
@@ -3,5 +3,5 @@ import { CreateEvent, Event, EventId } from '~shared/types'
 export interface IEventsRepository {
   getById(id: EventId): Promise<Event | undefined>
   getAll(): Promise<Event[]>
-  create(event: CreateEvent): Promise<void>
+  create(event: CreateEvent): Promise<EventId>
 }
