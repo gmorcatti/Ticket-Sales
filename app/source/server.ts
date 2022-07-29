0d96edda041d532ed56dfd5f52e2a4f6be716af3
Gabriel Morcatti <gabriel.morcatti@yahoo.com.br>
2022-07-29 09:04:18 -0300

    fix: Update ErrorHandler Import

--- app/source/server.ts
+++ app/source/server.ts
@@ -6,7 +6,7 @@ import express from 'express'
 
 import routes from './routes'
 
-import { ErrorHandler } from '~shared/types/errors/ErrorHandler'
+import { ErrorHandler } from '~source/shared/errors/ErrorHandler'
 
 const app = express()
 
