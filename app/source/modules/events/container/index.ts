24a04eb1f3d0d75d743d25472f251fe30291efda
Gabriel Morcatti <gabriel.morcatti@yahoo.com.br>
2022-08-02 18:18:01 -0300

    chore: Create test container

--- app/source/modules/events/container/index.ts
+++ app/source/modules/events/container/index.ts
@@ -2,12 +2,12 @@ import { container } from 'tsyringe'
 
 import { IEventsRepository } from '~source/modules/events/repositories/IEventsRepository'
 
-import { EventsRepository } from '~source/modules/events/repositories/implementatios/EventsRepositoryInMemory'
+import { EventsRepositoryPostgres } from '~source/modules/events/repositories/implementatios/EventsRepositoryPostgres'
 import { CreateEventService } from '~source/modules/events/services/CreateEvents.service'
 
 const eventsContainer = container.createChildContainer()
 
-eventsContainer.registerSingleton<IEventsRepository>('EventsRepository', EventsRepository)
+eventsContainer.registerSingleton<IEventsRepository>('EventsRepository', EventsRepositoryPostgres)
 
 eventsContainer.registerSingleton<CreateEventService>('CreateEventService', CreateEventService)
 
