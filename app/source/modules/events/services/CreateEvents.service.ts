48cd1ab324217a06b0fb99991965a167211679ef
Gabriel Morcatti <gabriel.morcatti@yahoo.com.br>
2022-08-02 18:17:17 -0300

    feat: Validate every required field on service

--- app/source/modules/events/services/CreateEvents.service.ts
+++ app/source/modules/events/services/CreateEvents.service.ts
@@ -10,8 +10,16 @@ export class CreateEventService {
   constructor (@inject('EventsRepository') private eventsRepository: IEventsRepository) {}
 
   async execute (event: CreateEvent) {
-    if (!event) {
-      throw new AppError('Event was not informed')
+    if (!event.name) {
+      throw new AppError('Event name was not informed')
+    }
+
+    if (!event.capacity) {
+      throw new AppError('Event capacity was not informed')
+    }
+
+    if (!event.date) {
+      throw new AppError('Event date was not informed')
     }
 
     return this.eventsRepository.create(event)
