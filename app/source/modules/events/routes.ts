6e558017c73dc6f348d945a36bffd3381c1fa3f7
Gabriel Morcatti <gabriel.morcatti@yahoo.com.br>
2022-08-03 09:35:17 -0300

    feat: Add body validation to route

--- app/source/modules/events/routes.ts
+++ app/source/modules/events/routes.ts
@@ -4,12 +4,15 @@ import { eventsContainer } from './container'
 
 import { CreateEventController } from './controllers'
 
+import { validateBody } from '~source/shared/middlewares/validateBody'
+import { eventSchema } from '~source/shared/validations/event'
+
 const routes = Router()
 
 const createEventController = eventsContainer.resolve<CreateEventController>(
   CreateEventController,
 )
 
-routes.post('/', createEventController.handle)
+routes.post('/', validateBody(eventSchema), createEventController.handle)
 
 export default routes
