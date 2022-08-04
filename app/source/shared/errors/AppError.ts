26ba88584c8bd0792aaf1a4205c344ffe26c8ab8
Gabriel Morcatti <gabriel.morcatti@yahoo.com.br>
2022-08-04 09:20:40 -0300

    feat: AppError accepts details

--- app/source/shared/errors/AppError.ts
+++ app/source/shared/errors/AppError.ts
@@ -1,10 +1,12 @@
 export class AppError extends Error {
   statusCode: number
+  details: object | undefined
 
-  constructor (message: string, statusCode = 400) {
+  constructor (message: string, statusCode = 400, details?: object) {
     super(message)
 
     this.message = message
     this.statusCode = statusCode
+    this.details = details
   }
 }
