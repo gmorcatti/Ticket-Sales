0d81ef479377931dd5c837610f4c3a2f062a8ab7
Gabriel Morcatti <gabriel.morcatti@yahoo.com.br>
2022-08-04 09:24:27 -0300

    feat: Add getEventById Route

--- app/source/modules/events/routes.ts
+++ app/source/modules/events/routes.ts
@@ -4,6 +4,8 @@ import { eventsContainer } from './container'
 
 import { CreateEventController } from './controllers'
 
+import { GetEventByIdController } from './controllers/getEventById.controller'
+
 import { validateRequest, ParamType } from '~source/shared/middlewares/validateRequest'
 import { eventSchema, eventIdSchema } from '~source/shared/validations/event'
 
@@ -13,10 +15,19 @@ const createEventController = eventsContainer.resolve<CreateEventController>(
   CreateEventController,
 )
 
+const getEventByIdController = eventsContainer.resolve<GetEventByIdController>(
+  GetEventByIdController,
+)
+
 routes.post(
   '/',
   validateRequest(ParamType.BODY, eventSchema),
   createEventController.handle,
+)
+routes.get(
+  '/:id',
+  validateRequest(ParamType.PARAMS, eventIdSchema),
+  getEventByIdController.handle,
 )
 
 export default routes
