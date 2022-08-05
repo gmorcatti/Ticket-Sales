b0669a4084172707e68e6b790cfd058c2a42c84a
Gabriel Morcatti <gabriel.morcatti@yahoo.com.br>
2022-08-05 09:09:21 -0300

    chore: Insert service to test container

--- app/tests/unit/modules/events/container/index.ts
+++ app/tests/unit/modules/events/container/index.ts
@@ -3,12 +3,15 @@ import { container } from 'tsyringe'
 import { IEventsRepository } from '~source/modules/events/repositories/IEventsRepository'
 
 import { EventsRepositoryInMemory } from '~source/modules/events/repositories/implementatios/EventsRepositoryInMemory'
+
 import { CreateEventService } from '~source/modules/events/services/CreateEvents.service'
+import { GetEventByIdService } from '~source/modules/events/services/GetEventById.serive'
 
 const eventsTestContainer = container.createChildContainer()
 
 eventsTestContainer.registerSingleton<IEventsRepository>('EventsRepository', EventsRepositoryInMemory)
 
 eventsTestContainer.registerSingleton<CreateEventService>('CreateEventService', CreateEventService)
+eventsTestContainer.registerSingleton<GetEventByIdService>('GetEventByIdService', GetEventByIdService)
 
 export { eventsTestContainer }
